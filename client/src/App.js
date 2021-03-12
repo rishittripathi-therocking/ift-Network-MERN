ab7a341a84d0937c630c215da10f4b4038e9bda6
rishittripathi-therocking <rishittripathi13@gmail.com>
2021-03-12 14:42:15 +0530

    Register/Login initialization

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router,  Route} from 'react-router-dom';
+import PageRender from './PageRender';
+
 function App() {
   return (
     <Router>
       <input type="checkbox" id="theme" />
       <div className="App">
-        <div class="main">
-          <h1>Hello</h1>
+        <div className="main">
+          <Route exact path='/:page' component={PageRender} />
+          <Route exact path='/:page/:id' component={PageRender} />
         </div>
       </div>
     </Router>
