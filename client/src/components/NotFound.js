1d5aff3a673611f16222d70539d944a16f7bd2f6
rishittripathi-therocking <rishittripathi13@gmail.com>
2021-03-12 14:54:43 +0530

    Done with day 1

--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const NotFound = () => {
     return (
-        <div>
-            NotFound
+        <div className="position-relative" style={{minHeight: 'calc(100vh - 70px)'}}>
+            <h2 className="position-absolute text-danger" style={{top:'50%', left:'50%', transform: 'translate(-50%,-50%)'}}>404 | Not Found</h2>
         </div>
     )
 }
