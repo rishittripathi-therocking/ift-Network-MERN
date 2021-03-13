e3b8185f6971b293e46a276aea42a023999b9384
rishittripathi-therocking <rishittripathi13@gmail.com>
2021-03-13 15:21:37 +0530

    Login Styling

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -20,28 +20,31 @@ const Login = () => {
         dispatch(login(userData))
     }
     return (
-        <div className="container auth_page" >
-            <h1 className="text-uppercase">IFT-NETWORK</h1>
-            <div className="card">
-                <article className="card-body">
-                    <Link to="/register" className="float-right btn btn-outline-primary text-danger">Register Now</Link>
-                    <h4 className="card-title mb-4 mt-1">Sign in</h4>
-                    <form onSubmit={handleSubmit}>
-                        <div className="form-group">
-                            <label htmlFor="emailInput">Your email</label>
-                            <input name="email" id="emailInput" className="form-control" placeholder="Email" type="email" onChange={handleChangeInput} value={email}/>
-                        </div> 
-                        <div className="form-group">
-                            <label htmlFor="inputPass">Your password</label>
-                            <input name="password" id="inputPass" className="form-control" placeholder="******" type="password" onChange={handleChangeInput} value={password}/>
-                        </div>
-                        <div className="form-group">
-                            <button type="submit" className="btn btn-primary btn-block" disabled={email&&password?false:true}> Login  </button>
-                        </div>                                                      
-                    </form>
-                </article>
-            </div> 
-        </div>
+        <React.Fragment>
+            
+            <div className="container auth_page" >
+                <div className="card">
+                    <h1 className="text-uppercase text-center">IFT-NETWORK</h1>
+                    <article className="card-body">
+                        <Link to="/register" className="float-right btn btn-outline-primary text-danger">Register Now</Link>
+                        <h4 className="card-title mb-4 mt-1" style={{fontSize: '33px'}}>Sign in</h4>
+                        <form onSubmit={handleSubmit}>
+                            <div className="form-group">
+                                <label htmlFor="emailInput">Your email</label>
+                                <input name="email" id="emailInput" className="form-control" placeholder="Email" type="email" onChange={handleChangeInput} value={email}/>
+                            </div> 
+                            <div className="form-group">
+                                <label htmlFor="inputPass">Your password</label>
+                                <input name="password" id="inputPass" className="form-control" placeholder="******" type="password" onChange={handleChangeInput} value={password}/>
+                            </div>
+                            <div className="form-group">
+                                <button type="submit" className="btn btn-primary btn-block" disabled={email&&password?false:true}> Login  </button>
+                            </div>                                                      
+                        </form>
+                    </article>
+                </div> 
+            </div>
+        </React.Fragment>
     )
 }
 
