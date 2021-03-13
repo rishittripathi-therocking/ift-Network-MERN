143850263ab31970b6321ff0808774f2e4a0d424
rishittripathi-therocking <rishittripathi13@gmail.com>
2021-03-13 13:59:08 +0530

    Half Login Form

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import {BrowserRouter as Router,  Route} from 'react-router-dom';
 import PageRender from './PageRender';
+import Home from './pages/home';
+import Login from './pages/login';
 
 function App() {
   return (
@@ -7,6 +9,7 @@ function App() {
       <input type="checkbox" id="theme" />
       <div className="App">
         <div className="main">
+          <Route exact path='/' component={Login} />
           <Route exact path='/:page' component={PageRender} />
           <Route exact path='/:page/:id' component={PageRender} />
         </div>
