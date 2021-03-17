90fc3ba77aa290143fad1650f64e3996104431ce
rishittripathi-therocking <rishittripathi13@gmail.com>
2021-03-17 18:16:57 +0530

    Status

--- client/src/components/StatusModal.js
+++ client/src/components/StatusModal.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {GLOBALTYPES} from '../redux/actions/globalType';
 
@@ -7,6 +7,10 @@ const StatusModal = () => {
     const dispatch = useDispatch();
     const [content, setContent] = useState('');
     const [images, setImages] = useState([]);
+    const [stream, setStream]= useState(false);
+    const videoRefrence = useRef();
+    const canvasRefrence = useRef();
+    const [tracks, setTracks] = useState('');
 
     const handleChangeImages = (e) => {
         const files = [...e.target.files];
@@ -32,6 +36,36 @@ const StatusModal = () => {
         setImages(newArr);
     }
 
+    const handleStream = ()=> {
+        setStream(true);
+        if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia){
+            navigator.mediaDevices.getUserMedia({video: true})
+            .then(mediaStream => {
+                videoRefrence.current.srcObject = mediaStream;
+                videoRefrence.current.play();
+                const track = mediaStream.getTracks();
+                setTracks(track[0])
+            }).catch(err => console.log(err))
+        }
+    }
+
+    const handleCapture = () => {
+        const width = videoRefrence.current.clientWidth;
+        const height = videoRefrence.current.clientHeight;
+
+        canvasRefrence.current.setAttribute("width", width);
+        canvasRefrence.current.setAttribute("height", height);
+        const ctx = canvasRefrence.current.getContext('2d');
+        ctx.drawImage(videoRefrence.current, 0, 0, width, height);
+        let URL = canvasRefrence.current.toDataURL();
+        setImages([...images, {camera: URL}])
+    }
+
+    const handleStopCameraStream = () => {
+        tracks.stop()
+        setStream(false);
+    }
+
     return (
         <div className="status_modal">
             <form>
@@ -46,17 +80,34 @@ const StatusModal = () => {
                 <div className="status_body">
                     <textarea name="content" placeholder={`${auth.user.username}, what are you thinking`} onChange={e => setContent(e.target.value)}/>
                     <div className="input_images">
-                        <i className="fas fa-camera"/>
-                        <div className="file_upload">
-                            <i className="fas fa-image"></i>
-                            <input type="file" name="file" id="file" multiple accept="image/*" onChange={handleChangeImages}/>
-                        </div>
+                        {
+                            stream ? 
+                            <i className="fas fa-camera" onClick={handleCapture}/>
+                            :
+                            <>
+                                <i className="fas fa-camera" onClick={handleStream}/>
+                                <div className="file_upload">
+                                    <i className="fas fa-image"></i>
+                                    <input type="file" name="file" id="file" multiple accept="image/*" onChange={handleChangeImages}/>
+                                </div>
+                            </>
+                        }
+                        
                     </div>
+
+                    {
+                        stream && <div className="stream position-relative">
+                            <video autoPlay muted style={{filter: theme ? 'invert(1)':'invert(0)'}} ref={videoRefrence} width="100%" height="100%" />
+                            <span onClick={handleStopCameraStream}>&times;</span>
+                            <canvas ref={canvasRefrence} style={{display: 'none'}}/>
+                        </div>
+                    }
+
                     <div className="show_images">
                         {
                             images.map((img,ind) => (
                                 <div key={ind} id="file_img">
-                                    <img src={URL.createObjectURL(img)} alt="images" className="img img-responsive img-thumbnail" style={{filter: theme ? 'invert(1)':'invert(0)'}}/>
+                                    <img src={img.camera ? img.camera : URL.createObjectURL(img)}  alt="images" className="img img-responsive img-thumbnail" style={{filter: theme ? 'invert(1)':'invert(0)'}}/>
                                     <span onClick={() => deleteImages(ind)}>&times;</span>
                                 </div>
                             ))
