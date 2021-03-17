af6fb9b06f38c6243724a513b0933a02464fecb5
rishittripathi-therocking <rishittripathi13@gmail.com>
2021-03-17 09:17:05 +0530

    patch route created

--- client/src/redux/actions/profileAction.js
+++ client/src/redux/actions/profileAction.js
@@ -55,6 +55,13 @@ export const follow = ({users,user,auth}) => async(dispatch) => {
     const newUser = {...user,followers: [...user.followers, auth.user]};
     dispatch({type: PROFILE_TYPES.FOLLOW, payload: newUser});
     dispatch({type: GLOBALTYPES.AUTH, payload: {...auth,user: {...auth.user, following: [...auth.user.following, newUser]}}});
+    try{
+        await patchDataAPI(`user/${user._id}/follow`,null,auth.token);
+
+    } catch(err) {
+        dispatch({type: GLOBALTYPES.ALERT, payload: {error: err.response.data.msg}});
+        dispatch({type:GLOBALTYPES.ALERT ,payload: {}});
+    }
 }
 
 export const unfollow = ({users,user,auth}) => async(dispatch) => {
@@ -62,4 +69,11 @@ export const unfollow = ({users,user,auth}) => async(dispatch) => {
     
     dispatch({type: PROFILE_TYPES.UNFOLLOW, payload: newUser});
     dispatch({type: GLOBALTYPES.AUTH, payload: {...auth,user: {...auth.user, following: DeleteData(auth.user.following, newUser._id)}}});
+    try{
+        await patchDataAPI(`user/${user._id}/unfollow`,null,auth.token);
+        
+    } catch(err) {
+        dispatch({type: GLOBALTYPES.ALERT, payload: {error: err.response.data.msg}});
+        dispatch({type:GLOBALTYPES.ALERT ,payload: {}});
+    }
 }
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -5,5 +5,7 @@ const userController = require('../controllers/userController');
 router.get('/search', auth,userController.searchUser);
 router.get('/user/:id',auth,userController.getUser);
 router.patch('/user',auth,userController.updateUser);
+router.patch('/user/:id/follow',auth,userController.follow);
+router.patch('/user/:id/unfollow',auth,userController.unfollow);
 
 module.exports = router;
