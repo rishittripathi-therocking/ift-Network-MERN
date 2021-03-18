ee79198a65552b8f95784e816996ec48090de8f2
rishittripathi-therocking <rishittripathi13@gmail.com>
2021-03-18 14:06:01 +0530

    Card Footer

--- client/src/components/home/CardFooter.js
+++ client/src/components/home/CardFooter.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import Send from '../../images/send.svg';
 
 const CardFooter = ({post}) => {
     return (
-        <div>
-            <h1>CardFooter</h1>
+        <div className="card_footer">
+            <div className="card_icon_menu">
+                <div>
+                    <i className="far fa-heart"/>
+                    <Link to={`/post/${post._id}`} className="text-dark">
+                        <i className="far fa-comment"/>
+                    </Link>
+                    <img src={Send} alt="send" />
+                </div>
+                <i className="far fa-bookmark"/>
+            </div>
+            <div className="row justify-content-between mx-0">
+                <h6 style={{padding: '0 25px', cursor:'pointer'}}>
+                    {post.likes.length} likes
+                 </h6>
+                <h6 style={{padding: '0 25px', cursor:'pointer'}}>
+                    {post.comments.length} comments
+                </h6>
+            </div>
         </div>
     )
 }
