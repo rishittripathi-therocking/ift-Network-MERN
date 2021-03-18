3f21ef0cfb015abb89f8e7fdae5e6d6cb5d98cfd
rishittripathi-therocking <rishittripathi13@gmail.com>
2021-03-18 12:57:52 +0530

    Card Body Done

--- client/src/components/home/CardBody.js
+++ client/src/components/home/CardBody.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Carousel from '../Carousel';
 
 const CardBody = ({post}) => {
+    const [readMore, setReadMore] = useState(false);
+    
+    
+    
     return (
         <div className="card_body">
             <div className="card_body-content">
-                <span>{post.content}</span>
+                <span>
+                    {
+                        post.content.length < 60 ? post.content :
+                        readMore ? post.content + ' ' : post.content.slice(0,60) + '.....'
+                    }
+                </span>
+                {
+                    post.content.length > 60 &&
+                    <span className="readMore" onClick={() => setReadMore(!readMore)}>
+                        {readMore ? 'Hide content' : 'Read More'}
+                    </span>
+                }
             </div>
             {
                 post.images.length > 0 && <Carousel images={post.images} id={post._id}/>
