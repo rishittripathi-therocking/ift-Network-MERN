d25510bc6134511b848f57c3679885aee149b745
rishittripathi-therocking <rishittripathi13@gmail.com>
2021-03-31 07:35:37 +0530

    Comment Reply View Display

--- client/src/components/home/commentDisplay.js
+++ client/src/components/home/commentDisplay.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import CommentsCard from './commentCard';
 
 const CommentDisplay = ({comment, post,replyComment}) => {
-     
+    const [viewComment,setViewComment] = useState(false);
     return (
         <div className="comment_display">
             <CommentsCard comment={comment} post={post} key={post._id} commentId={comment._id} >
-                <div className="pl-4">
+                <h6 className="pt-2" onClick={()=> setViewComment(!viewComment)} style={{color:'crimson', paddingLeft:'23px', cursor:'pointer'}}>
                     {
-                        replyComment.map((item,ind)=>(
-                            item.reply && 
-                            <CommentsCard key={ind} comment={item} post={post} commentId={comment._id} />
-                        ))
+                        viewComment ? 'Hide All Reply Comments ' : `View all ${replyComment.length} Reply Comments ... `
                     }
-                </div>
+                </h6>
+                {
+
+                    viewComment &&
+                    <div className="pl-4 pt-2">
+                        {
+                            replyComment.map((item,ind)=>(
+                                item.reply && 
+                                <CommentsCard key={ind} comment={item} post={post} commentId={comment._id} />
+                            ))
+                        }
+                    </div>
+                }
             </CommentsCard>
         </div>
     )
