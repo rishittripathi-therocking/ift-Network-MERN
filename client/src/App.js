0a3347f6639fd525620983c0b087f5558e52888a
Rishit Tripathi <51650467+rishittripathi-therocking@users.noreply.github.com>
2023-03-03 00:42:07 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -27,7 +27,7 @@ function App() {
   const {auth, status,modal, call} = useSelector(state => state);
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(refreshToken());
+    dispatch(refreshToken(auth?.token));
     const socket = io();
     dispatch({type: GLOBALTYPES.SOCKET, payload: socket})
     return ()=> socket.close()
