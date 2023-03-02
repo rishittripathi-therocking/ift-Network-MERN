56e423e9cc9b52cfbce56e55f52f2c9f4f4acb59
Rishit Tripathi <51650467+rishittripathi-therocking@users.noreply.github.com>
2023-03-02 22:45:11 +0530

    Update fetchData.js

--- client/src/utils/fetchData.js
+++ client/src/utils/fetchData.js
@@ -1,36 +1,37 @@
 import axios from 'axios';
 
+
 export const getDataAPI = async(url, token) => {
     
-    const res = await axios.get(`/socialapi/${url}`,{
+    const res = await axios.get(`${process.env.BASE_URL}/socialapi/${url}`,{
         headers: {Authorization: token}
     })
     return res;
 }
 
 export const postDataAPI = async(url,post, token) => {
-    const res = await axios.post(`/socialapi/${url}`,post,{
+    const res = await axios.post(`${process.env.BASE_URL}/socialapi/${url}`,post,{
         headers: {Authorization: token}
     })
     return res;
 }
 
 export const putDataAPI = async(url,post, token) => {
-    const res = await axios.put(`/socialapi/${url}`,post,{
+    const res = await axios.put(`${process.env.BASE_URL}/socialapi/${url}`,post,{
         headers: {Authorization: token}
     })
     return res;
 }
 
 export const patchDataAPI = async(url,post, token) => {
-    const res = await axios.patch(`/socialapi/${url}`,post,{
+    const res = await axios.patch(`${process.env.BASE_URL}/socialapi/${url}`,post,{
         headers: {Authorization: token}
     })
     return res;
 }
 
 export const deleteDataAPI = async(url, token) => {
-    const res = await axios.delete(`/socialapi/${url}`,{
+    const res = await axios.delete(`${process.env.BASE_URL}/socialapi/${url}`,{
         headers: {Authorization: token}
     })
     return res;
